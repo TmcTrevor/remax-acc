4899b2d0c770439b115427336146c23e7ae1c331
tmctrevor <mohamed.james1949@gmail.com>
2025-01-08 11:22:23 +0100

    fix the choices in all Listing

--- src/app/components/AllListingHeader.tsx
+++ src/app/components/AllListingHeader.tsx
@@ -63,10 +63,9 @@ const AllListingHeader = () => {
         <div className="flex flex-col px-48 lg:px-0 lg:flex-row items-center justify-center w-full bg-white shadow-lg rounded-r-lg">
           {/* Search Inputs */}
           <div className="flex flex-col  lg:flex-row items-center w-full p-4 gap-4 lg:gap-0">
-            {/* Location Input */}
+            {/* Location Select */}
             <div className="relative min-h-[120px] w-full lg:w-1/3 p-4 flex flex-col hover:bg-gray-100 justify-center border-r border-gray-300 items-start group">
               <label
-                htmlFor="location"
                 className={`absolute text-3xl font-medium text-gray-700 cursor-pointer transition-all duration-300 ease-in-out 
                   ${
                     location
@@ -75,13 +74,61 @@ const AllListingHeader = () => {
                   } group-focus-within:top-0 group-focus-within:translate-y-0 group-focus-within:scale-75 group-focus-within:text-blue-500 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100`}>
                 Location
               </label>
-              <input
-                id="location"
-                type="text"
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-                className="w-full px-4 py-2 border-0 focus:border-transparent bg-transparent focus:outline-none focus:ring-0 text-gray-900 transition-all duration-300 ease-in-out peer"
-              />
+              <Select onValueChange={(value) => setLocation(value)}>
+                <SelectTrigger className="relative text-gray-900 w-full !border-none !focus:border-none bg-transparent focus:ring-0 flex justify-between items-center">
+                  <SelectValue
+                    placeholder=""
+                    className="text-gray-900 peer text-lg mt-2"
+                  />
+                </SelectTrigger>
+                <SelectContent className="z-50 mt-2 border border-gray-200 rounded-lg shadow-lg bg-white text-gray-900 text-sm">
+                  <SelectItem
+                    value="Ambergris Caye"
+                    className="px-4 py-2 hover:bg-gray-100 transition-colors">
+                    Ambergris Caye
+                  </SelectItem>
+                  <SelectItem
+                    value="Belize City"
+                    className="px-4 py-2 hover:bg-gray-100 transition-colors">
+                    Belize City
+                  </SelectItem>
+                  <SelectItem
+                    value="Belmopan"
+                    className="px-4 py-2 hover:bg-gray-100 transition-colors">
+                    Belmopan
+                  </SelectItem>
+                  <SelectItem
+                    value="Caye Caulker"
+                    className="px-4 py-2 hover:bg-gray-100 transition-colors">
+                    Caye Caulker
+                  </SelectItem>
+                  <SelectItem
+                    value="Corozal"
+                    className="px-4 py-2 hover:bg-gray-100 transition-colors">
+                    Corozal
+                  </SelectItem>
+                  <SelectItem
+                    value="Hopkins"
+                    className="px-4 py-2 hover:bg-gray-100 transition-colors">
+                    Hopkins
+                  </SelectItem>
+                  <SelectItem
+                    value="Placencia"
+                    className="px-4 py-2 hover:bg-gray-100 transition-colors">
+                    Placencia
+                  </SelectItem>
+                  <SelectItem
+                    value="San Ignacio"
+                    className="px-4 py-2 hover:bg-gray-100 transition-colors">
+                    San Ignacio
+                  </SelectItem>
+                  <SelectItem
+                    value="San Pedro"
+                    className="px-4 py-2 hover:bg-gray-100 transition-colors">
+                    San Pedro
+                  </SelectItem>
+                </SelectContent>
+              </Select>
             </div>
 
             {/* Property Type Select */}
@@ -104,17 +151,22 @@ const AllListingHeader = () => {
                 </SelectTrigger>
                 <SelectContent className="z-50 mt-2 border border-gray-200 rounded-lg shadow-lg bg-white text-gray-900 text-sm">
                   <SelectItem
-                    value="house"
+                    value="Homes"
                     className="px-4 py-2 hover:bg-gray-100 transition-colors">
-                    House
+                    Homes
                   </SelectItem>
                   <SelectItem
-                    value="apartment"
+                    value="Condos"
                     className="px-4 py-2 hover:bg-gray-100 transition-colors">
-                    Apartment
+                    Condos
                   </SelectItem>
                   <SelectItem
-                    value="land"
+                    value="Commercial"
+                    className="px-4 py-2 hover:bg-gray-100 transition-colors">
+                    Commercial
+                  </SelectItem>
+                  <SelectItem
+                    value="Land"
                     className="px-4 py-2 hover:bg-gray-100 transition-colors">
                     Land
                   </SelectItem>
